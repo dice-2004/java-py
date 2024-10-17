d2927485ca335bdd93cab8a5ea5ea93bcae63347
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-10-17 11:53:46 +0900

    fixed error

--- yoshu.py
+++ yoshu.py
@@ -1,19 +1,51 @@
 import pyperclip
+import sys
 
 def xxx():
-    x, y = map(int, input().split())
+    while True:
+        try:
+            x, y = map(int, input("Please enter the question numbers (lower and upper limits).\n >> ").split())
+            if x > y:
+                print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n   Please enter the correct format.\n    ex) 0 2\n   0 is minimum, 2 is maximum.\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+                continue
+            break
+        except ValueError:
+            print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n   Please enter the correct format.\n    ex) 0 2\n   0 is minimum, 2 is maximum.\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+            continue
+        except KeyboardInterrupt:
+            print("\n   exit program.\n\n\n\n")
+            sys.exit()
+
+
+
     Z = []
     result=""
     for c in range(x, y + 1):
-        print(f"{c}-> ", end="")
-        Z.append(input())
+        try:
+            Z.append(input(f"{c}-> "))
+        except KeyboardInterrupt:
+            print("\n   exit program.\n\n\n\n")
+            sys.exit()
+
 
     for a, b in zip(range(x, y + 1), Z):
-        result+= (f"({a}){b}")
+        result+= (f"\n({a}){b}")
 
     print(result)
+    print("\n   result is copied to clipboard.\n")
     pyperclip.copy(result)
+
+
 if __name__ == '__main__':
     while True:
         xxx()
+        try:
+            i=list(map(int,input(f"0 is exit , other is comtinue.\n >>")))
+        except KeyboardInterrupt:
+            print("\n   exit program.\n\n\n\n")
+            sys.exit()
+
+        if i[0]==0:
+            print("\n   exit program.\n\n\n\n")
+            sys.exit()
         print("\n")
